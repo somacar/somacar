f1779f337c50d838072555950c2f3b94ea0e1d64
YoungIn Lee <pimery@nate.com>
2016-04-24 00:29:34 +0900

    qrcode/

---
+++ raspberry/opencv/qrcode/FinderPatternTrio.h
@@ -0,0 +1,18 @@
+#ifndef __FINDER_PATTERN_INFO
+#define __FINDER_PATTERN_INFO
+
+#include "FinderPattern.h"
+
+class FinderPatternTrio {
+private:
+    FinderPattern *bottomLeft;
+    FinderPattern *topLeft;
+    FinderPattern *topRight;
+
+public:
+    FinderPattern* getBottomLeft();
+    FinderPattern* getTopLeft();
+    FinderPattern* getTopRight();
+    FinderPatternTrio(FinderPattern**);
+};
+#endif
