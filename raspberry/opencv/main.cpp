8d20469f1ad352ab69859ff9cef378e771d95dd6
YoungIn Lee <pimery@nate.com>
2016-04-24 00:30:52 +0900

    main.cpp: test/webcam_demo.cpp

--- raspberry/opencv/main.cpp
+++ raspberry/opencv/main.cpp
@@ -1,86 +1,26 @@
-#include <opencv2/core/core.hpp>
+#include <stdio.h>
+#include <vector>
 #include <opencv2/highgui/highgui.hpp>
-#include <opencv2/features2d/features2d.hpp>
-#include <opencv2/xfeatures2d.hpp>
-#include <opencv2/calib3d/calib3d.hpp>
-#include <opencv2/flann/flann.hpp>
-#include <opencv2/imgproc.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+#include "qrReader.h"
 
+using namespace std;
 using namespace cv;
 
-#define RED Scalar(0, 0, 255)
+int main() {
+    VideoCapture v(0);
+    assert(v.isOpened());
 
-int main(int argc, char *argv[]) {
-    Mat img1 = imread("stop.png", CV_LOAD_IMAGE_GRAYSCALE);
-    Mat img2 = imread("scene.JPG", CV_LOAD_IMAGE_GRAYSCALE);
-    assert(img1.data && img2.data);
+    qrReader qr = qrReader();
+    Mat frame, gray;
+    while(true) {
+        v.read(frame);
 
-    cv::Ptr<Feature2D> detector = xfeatures2d::SURF::create(0.3);
-    std::vector<KeyPoint> key1, key2;
+        cvtColor(frame, gray, CV_BGR2GRAY);
+        threshold(gray, gray, 128, 255, THRESH_BINARY);
 
-    detector->detect(img1, key1);
-    detector->detect(img2, key2);
-
-    Mat disp;
-    drawKeypoints(img1, key1, disp, RED, DrawMatchesFlags::DRAW_RICH_KEYPOINTS);
-    namedWindow("키포인트");
-    imshow("키포인트", disp);
-
-    cv::Ptr<Feature2D> extractor = xfeatures2d::SURF::create();
-    Mat des1, des2;
-    extractor->compute(img1, key1, des1);
-    extractor->compute(img2, key2, des2);
-
-    FlannBasedMatcher matcher;
-    std::vector<DMatch> match;
-    matcher.match(des1, des2, match);
-
-    double maxd = 0;
-    double mind = 100;
-    for (int i = 0; i < des1.rows; i++) {
-        double dist = match[i].distance;
-        if (dist < mind) mind = dist;
-        if (dist > maxd) maxd = dist;
+        bool found = qr.find(gray);
+        if(found)
+            qr.draw(gray);
     }
-
-    std::vector<DMatch> good_match;
-    for (int i = 0; i < des1.rows; i++)
-        if (match[i].distance <= max(2 * mind, 0.02)) good_match.push_back(match[i]);
-
-    Mat img_match;
-    drawMatches(img1, key1, img2, key2, good_match, img_match, Scalar::all(-1), Scalar::all(-1), std::vector<char>(),
-                DrawMatchesFlags::NOT_DRAW_SINGLE_POINTS);
-
-    namedWindow("매칭 결과");
-    imshow("매칭 결과", img_match);
-
-    for (int i = 0; i < (int) good_match.size(); i++)
-        printf("키포인트 %d~%d\n", good_match[i].queryIdx, good_match[i].trainIdx);
-
-    std::vector<Point2f> model_pt;
-    std::vector<Point2f> scene_pt;
-    for (int i = 0; i < good_match.size(); i++) {
-        model_pt.push_back(key1[good_match[i].queryIdx].pt);
-        scene_pt.push_back(key2[good_match[i].trainIdx].pt);
-    }
-
-    Mat H = findHomography(model_pt, scene_pt, CV_RANSAC);
-
-    std::vector<Point2f> model_corner(4);
-    model_corner[0] = cvPoint(0, 0);
-    model_corner[1] = cvPoint(img1.cols, 0);
-    model_corner[2] = cvPoint(img1.cols, img1.rows);
-    model_corner[3] = cvPoint(0, img1.rows);
-
-    std::vector<Point2f> scene_corner(4);
-    perspectiveTransform(model_corner, scene_corner, H);
-
-    Point2f p(img1.cols, 0);
-    line(img_match, scene_corner[0]+p, scene_corner[1]+p, RED, 3);
-    line(img_match, scene_corner[1]+p, scene_corner[2]+p, RED, 3);
-    line(img_match, scene_corner[2]+p, scene_corner[3]+p, RED, 3);
-    line(img_match, scene_corner[3]+p, scene_corner[0]+p, RED, 3);
-
-    waitKey(0);
-    return 0;
 }
