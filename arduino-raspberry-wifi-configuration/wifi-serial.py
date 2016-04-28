33b416db3ebca25811f618b81d4231f599df6acc
DongGyu Kim <rainc.root@gmail.com>
2016-04-29 04:27:12 +0900

    network 연동

--- arduino-raspberry-wifi-configuration/wifi-serial.py
+++ arduino-raspberry-wifi-configuration/wifi-serial.py
@@ -1,11 +1,16 @@
 import serial
 import time
+from subprocess import call
 
-port = "/dev/ttyACM0"
-p_sr = serial.Serial(port,9600)
+port_location = "/dev/ttyACM0"
+wpa_configure_file = "/etc/wpa.config"
+
+
+p_sr = serial.Serial(port_location,9600)
 p_sr.flushInput()
 
 ap_mode = False
+
 
 while True:
     debug = str(p_sr.readline())
@@ -21,6 +26,12 @@ while True:
     	ap_mode = True
 
     if (ap_mode == True and not(process[0] == "AP_SEND")):
-    	print("AP_NAME", process[0])
-    	print("AP_pass", process[1])
+    	print("AP Set process..")
+    	print("---------------------------------")
+    	result = call(["wpa_passphrase", process[0], process[1]])
+    	print("[ROOT] Saving configure file")
+    	call("wpa_passphrase " + process[0] + " " + process[1] + "> " + wpa_configure_file ,shell=True)
+    	print("[ROOT]network service restarting")
+    	call("service networking restart",shell=True)
+    	print("[ROOT]Restart Completed")
     	ap_mode = False
