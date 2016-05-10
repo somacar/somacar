73962aa25e41d63d287cf44d3c3217a4eb95d302
YoungIn Lee <pimery@nate.com>
2016-05-10 22:45:07 +0900

    project/main.cpp: tess

--- raspberry/opencv/project/main.cpp
+++ raspberry/opencv/project/main.cpp
@@ -1,12 +1,16 @@
-#include "target.h"
+#include "main.hpp"
+#include "target.hpp"
+#include "ocr.hpp"
 
 using namespace cv;
 
 int main(int argc, char *argv[]) {
-//    VideoCapture v("FlightDemo.mp4");
     VideoCapture v(0);
     Mat frame;
+    UMat sqr;
     Target target;
+    OCRTess tess(true, REG_MSER, GR_EX);
+    tess.init(10);
 
 //    frame = imread("sam.jpg");
 //    target.init(frame);
@@ -17,7 +21,11 @@ int main(int argc, char *argv[]) {
     while (true) {
         v.read(frame);
         target.init(frame.getUMat(ACCESS_READ));
-        target.detect();
+        if (target.find_square(&sqr)) {
+            tess.set(sqr);
+            tess.detectAndRecog();
+            tess.show();
+        }
         target.show();
         if ((char) waitKey(1) == 27) break;
     }
