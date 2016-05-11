ed721f747f80167a48f74b603c2aed07b91b8098
YoungIn Lee <pimery@nate.com>
2016-05-11 16:57:06 +0900

    project/main.cpp: loop

--- raspberry/opencv/project/main.cpp
+++ raspberry/opencv/project/main.cpp
@@ -23,8 +23,9 @@ int main(int argc, char *argv[]) {
         target.init(frame.getUMat(ACCESS_READ));
         if (target.find_square(&sqr)) {
             tess.set(sqr);
-            tess.detectAndRecog();
-            tess.show();
+            tess.loop();
+//            tess.detectAndRecog();
+//            tess.show();
         }
         target.show();
         if ((char) waitKey(1) == 27) break;
