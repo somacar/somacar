2100ce0ec48af227b274b7ed42ae33f744786663
DongGyu Kim <rainc.root@gmail.com>
2016-05-12 21:11:49 +0900

    Test code commit (예외 처리 테스트 코드)

--- arduino-raspberry-wifi-connect/wifi-serial.py
+++ arduino-raspberry-wifi-connect/wifi-serial.py
@@ -23,14 +23,23 @@ def get_return_code_of_simple_cmd(cmd, stderr=STDOUT):
     args = shlex.split(cmd)
     return call(args, stdout=PIPE, stderr=stderr)
 
-def is_network_alive():
-    cmd = "ping -c 1 www.google.com"
+def is_network_alive(gateway_ip):
+    cmd = "ping -c 1 " + gateway_ip
     return get_return_code_of_simple_cmd(cmd) == 0
 
 def timeout( p ):
     if p.poll() is None:
         print ("Error: process taking too long to complete--terminating")
         p.kill()
+
+def runProcess(exe): #execute System command
+	p = subprocess.Popen(exe, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+	while(True):
+		retcode = p.poll() #returns None while subprocess is running
+		line = p.stdout.readline()
+		yield line
+		if(retcode is not None):
+			break
 
 
 while True:
@@ -54,23 +63,46 @@ while True:
 	    	print("[ROOT] Saving configure file")
 	    	call("wpa_passphrase " + process[0] + " " + process[1] + "> " + wpa_configure_file ,shell=True)
 	    	print("[ROOT]network service restarting")
-	    	signal.signal(signal.SIGALRM, signal_handler)
-	    	signal.alarm(20)   # Ten seconds limit for Network restart
+	    	# signal.signal(signal.SIGALRM, signal_handler)
+	    	# signal.alarm(20)   # Ten seconds limit for Network restart
 	    	try:
-	    	    	call("service networking restart",shell=True)
+	    	    	call("service networking stop",shell=True)
 	    	except Exception:
-	    	    	print("[ROOT] Network Timeout ")
-		
-	    	print("[ROOT]Restart Completed")
-	    	signal.alarm(0)
-	    	if (is_network_alive() == True):
+	    	    	print("[ROOT] command error")
+			
+			count = 0
+			ip_set = False
+			gateway_ip = ""
+
+
+			try:
+				for line in runProcess("service networking restart")
+					 if line.find("DHCPDISCOVER"):
+					 	count = count + 1
+
+					 if count == 5:
+					 	print("[ROOT] Cannot connect to AP")
+					 	break
+
+					 if line.find("DHCPACK"):
+					 	split_string = line.split("from ")
+					 	gateway_ip = split_string[1]
+					 	ip_set = True
+
+			except:
+				print("[ROOT] Unexcepted error occured")
+
+
+	    	if (is_network_alive(gateway_ip)):
 	    	    	p_sr.write(bytes("1", 'UTF-8'))
 	    	    	print("[ROOT] Wi-Fi Connected")
-	    	else:
+	    	
+	    	if (gateway_ip == "")
 	    	    	p_sr.write(bytes("2", 'UTF-8'))
 	    	    	print("[ROOT] Failed to connect.")
-	
+
 	    	ap_mode = False
+
     except serial.SerialException as e:
         print('OS Error')
     except TypeError as e:
