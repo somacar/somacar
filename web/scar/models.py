ce958036feae12c94ce742f26f03f4aaba67fc19
YoungIn Lee <pimery@nate.com>
2016-05-20 16:26:11 +0900

    scar/models.py: Location

--- web/scar/models.py
+++ web/scar/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from geoposition.fields import GeopositionField
 
-# Create your models here.
+
+class Location(models.Model):
+    pos = GeopositionField()
+
+    def __str__(self):
+        return self.pos
