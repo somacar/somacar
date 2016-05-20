f751575f731eb84c9d42acee04bee9a941a3709f
YoungIn Lee <pimery@nate.com>
2016-05-20 17:33:39 +0900

    scar/models.py: datetime

--- web/scar/models.py
+++ web/scar/models.py
@@ -4,6 +4,7 @@ from geoposition.fields import GeopositionField
 
 class Location(models.Model):
     pos = GeopositionField()
+    dt = models.DateField(auto_now=True)
 
     def __str__(self):
-        return self.pos
+        return self.dt
