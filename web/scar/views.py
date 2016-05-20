28f12afc9852bc2cd9f2da19b5bd01897f7f94f8
YoungIn Lee <pimery@nate.com>
2016-05-20 23:42:52 +0900

    web/scar/views.py: img_view

--- web/scar/views.py
+++ web/scar/views.py
@@ -6,3 +6,7 @@ from scar.models import *
 def map_view(request):
     loc = Location.objects.all()
     return render_to_response('map.html', {"loc": loc})
+
+
+def img_view(request):
+    return render_to_response('image.html')
