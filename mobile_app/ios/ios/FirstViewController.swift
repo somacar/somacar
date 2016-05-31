3470b02e4a33c070a92515eeb533f99a939454e3
YoungIn Lee <pimery@nate.com>
2016-05-31 12:36:09 +0900

    mobile_app/ios/

---
+++ mobile_app/ios/ios/FirstViewController.swift
@@ -0,0 +1,23 @@
+//
+//  FirstViewController.swift
+//  ios
+//
+//  Created by SINO on 2016. 5. 21..
+//  Copyright © 2016년 soma. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
+
