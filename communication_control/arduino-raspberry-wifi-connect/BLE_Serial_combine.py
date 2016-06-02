6d2289e1e158b5b9c06775562380aa0017f00b6d
DongGyu Kim <rainc.root@gmail.com>
2016-06-03 04:16:21 +0900

    Update Python indentation

--- communication_control/arduino-raspberry-wifi-connect/BLE_Serial_combine.py
+++ communication_control/arduino-raspberry-wifi-connect/BLE_Serial_combine.py
@@ -17,35 +17,21 @@ BT_ap_mode = False
 Serial_Location_port = "/dev/ttyACM0"
 Serial_wpa_configure_file = "/etc/wpa.config"
 
+
 def errorHandler(signum, stack):
     raise IOError("")
-
 def runProcess(p): #execute System command
-        yield "pid=" + str(os.getpgid(p.pid))
-        while(True):
-                retcode = p.poll() #returns None while subprocess is running
-                line = p.stdout.readline()
-                yield line
-                if(retcode is not None):
-                        networkProc_pid = ""
-                        break
-
-def SerialReceiver():
-	p_sr = serial.Serial(port_location,9600)
-	p_sr.flushInput()
-	while True:
-    try:
-	    debug = str(p_sr.readline())
-	    test = debug.replace(r"\r\n" ,"")
-	    test3 = test.replace("b'", "")
-	    SerialString = test3.replace("'" , "")
-	    # must be APName/APPass    
-	    print(SerialString)
-
-
+	yield "pid=" + str(os.getpgid(p.pid))
+	while(True):
+		retcode = p.poll() #returns None while subprocess is running
+		line = p.stdout.readline()
+		yield line
+		if(retcode is not None):
+			networkProc_pid = ""
+			break
 def BluetoothReceiver():
-	# Init bluetooth for receive
 	p = subprocess.Popen("sudo hciconfig hci0 piscan", stdout=subprocess.PIPE, stderr=subprocess.STDOUT,shell=True)
+	# Init bluetooth for receive
 	server_sock=bluetooth.BluetoothSocket( bluetooth.RFCOMM )
 	port = 1
 	server_sock.bind(("",port))
@@ -59,7 +45,7 @@ def BluetoothReceiver():
 			data = client_sock.recv(1024)
 			command_str = str(data).split("'")[1]
 			# client_sock.send(data)
-			print(hex(command_str))
+			# print(command_str.encode("hex"))
 
 			bluetoothHandler(command_str, client_sock)
 
@@ -67,7 +53,6 @@ def BluetoothReceiver():
 		print("Error", e)
 		client_sock.close()
 		server_sock.close()
-
 def bluetoothHandler(value, client_sock):
 	if value == "wifi":
 		print("Wi-Fi Mode Enabled")
@@ -76,55 +61,67 @@ def bluetoothHandler(value, client_sock):
 	if BT_ap_mode == True and not(value == "wifi"):
 		process = value.split("/")
 		print("AP Set process..")
-    	print("---------------------------------")
-    	result = call(["wpa_passphrase", process[0], process[1]])
-    	print("[ROOT] Saving configure file")
-    	call("wpa_passphrase " + process[0] + " " + process[1] + "> " + Serial_wpa_configure_file ,shell=True)
+		print("---------------------------------")
+		result = call(["wpa_passphrase", process[0], process[1]])
+		print("[ROOT] Saving configure file")
+		call("wpa_passphrase " + process[0] + " " + process[1] + "> " + Serial_wpa_configure_file ,shell=True)
 
-    	p = subprocess.Popen("service networking restart", stdout=subprocess.PIPE, stderr=subprocess.STDOUT,shell=True,preexec_fn=os.setsid)
-    	try:
-	    	for line in runProcess(p):
-	    		command_line = str(line)
-	    		if command_line.find("pid") == 0:
-	    			signal.signal(signal.SIGALRM, errorHandler)
-	    			signal.alarm(15)
+		p = subprocess.Popen("service networking restart", stdout=subprocess.PIPE, stderr=subprocess.STDOUT,shell=True,preexec_fn=os.setsid)
+		try:
+			for line in runProcess(p):
+				command_line = str(line)
+				if command_line.find("pid") == 0:
+					signal.signal(signal.SIGALRM, errorHandler)
+					signal.alarm(15)
 
-	    		if command_line.find("DHCPDISCOVER") ==0:
-	    			print('finded')
-	    			count = count + 1
-	    			if command_line.find("DHCPACK") == 0:
-	    				signal.alarm(0) # Alarm Off
-	    				print('command_line',command_line)
-	    				split_string = command_line.split("from ")
-	    				gateway_ip = split_string[1]
-	    				ip_set = True
-	    				print("[ROOT] gateway_ip : ",gateway_ip)
+				if command_line.find("DHCPDISCOVER") ==0:
+					print('finded')
+					count = count + 1
+					if command_line.find("DHCPACK") == 0:
+						signal.alarm(0) # Alarm Off
+						print('command_line',command_line)
+						split_string = command_line.split("from ")
+						gateway_ip = split_string[1]
+						ip_set = True
+						print("[ROOT] gateway_ip : ",gateway_ip)
 
-	    	p = subprocess.Popen("ifconfig wlan0", stdout=subprocess.PIPE, stderr=subprocess.STDOUT,shell=True,preexec_fn=os.setsid)
-	    	for line in runProcess(p):
-	    		command_line = str(line)
-	    		if (command_line.find('cast:') == 36):
-	    			split1 = command_line.split(' ')
-	    			split2 = split1[11].split(':')
-	    			ipaddr = split2[1]
+			p = subprocess.Popen("ifconfig wlan0", stdout=subprocess.PIPE, stderr=subprocess.STDOUT,shell=True,preexec_fn=os.setsid)
+			for line in runProcess(p):
+				command_line = str(line)
+				if (command_line.find('cast:') == 36):
+					split1 = command_line.split(' ')
+					split2 = split1[11].split(':')
+					ipaddr = split2[1]
 
-	    	#ipaddr = gateway Address
+			#ipaddr = gateway Address
 
-	    	if (is_network_alive(ipaddr)): # ping to gateway address (alive check)
-	    		client_sock.send("[BT] Wi-Fi Connected")
-	    		print("[ROOT] Wi-Fi Connected")
-	    	else:
-	    		client_sock.send("[BT] Failed to connect")
-	    		print("[ROOT] Wi-Fi Connected")
+			if (is_network_alive(ipaddr)): # ping to gateway address (alive check)
+				client_sock.send("[BT] Wi-Fi Connected")
+				print("[ROOT] Wi-Fi Connected")
+			else:
+				client_sock.send("[BT] Failed to connect")
+				print("[ROOT] Wi-Fi Connected")
 
 
-	    except IOError: #Limit 15 Seconds
+		except IOError: #Limit 15 Seconds
 			os.killpg(os.getpgid(p.pid), signal.SIGTERM)
 			print("Process Terminiated by Timeout (15 seconds)")
 			ap_mode = False
 
-    	ap_mode = False
+		ap_mode = False
 
+
+def SerialReceiver():
+	p_sr = serial.Serial(port_location,9600)
+	p_sr.flushInput()
+	while True:
+		try:
+			debug = str(p_sr.readline())
+			test = debug.replace(r"\r\n" ,"")
+			test3 = test.replace("b'", "")
+			SerialString = test3.replace("'" , "")
+			print(SerialString)
+		except:
+			print("")
 
 BluetoothReceiver()
-
