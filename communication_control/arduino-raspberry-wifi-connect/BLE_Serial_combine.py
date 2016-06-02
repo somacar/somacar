f1ce30167ef9abe09204cef8b73b409341f29ff1
DongGyu Kim <rainc.root@gmail.com>
2016-06-03 04:02:04 +0900

    test code commit

--- communication_control/arduino-raspberry-wifi-connect/BLE_Serial_combine.py
+++ communication_control/arduino-raspberry-wifi-connect/BLE_Serial_combine.py
@@ -8,9 +8,17 @@ import time
 import shlex
 import os
 
+
+#Bluetooth config
 BT_ap_mode = False
 
+
+#Serial config 
 Serial_Location_port = "/dev/ttyACM0"
+Serial_wpa_configure_file = "/etc/wpa.config"
+
+def errorHandler(signum, stack):
+    raise IOError("")
 
 def runProcess(p): #execute System command
         yield "pid=" + str(os.getpgid(p.pid))
@@ -50,7 +58,8 @@ def BluetoothReceiver():
 		while 1:
 			data = client_sock.recv(1024)
 			command_str = str(data).split("'")[1]
-			client_sock.send(data)
+			# client_sock.send(data)
+			print(hex(command_str))
 
 			bluetoothHandler(command_str, client_sock)
 
@@ -60,7 +69,6 @@ def BluetoothReceiver():
 		server_sock.close()
 
 def bluetoothHandler(value, client_sock):
-	
 	if value == "wifi":
 		print("Wi-Fi Mode Enabled")
 		BT_ap_mode = True
@@ -71,6 +79,52 @@ def bluetoothHandler(value, client_sock):
     	print("---------------------------------")
     	result = call(["wpa_passphrase", process[0], process[1]])
     	print("[ROOT] Saving configure file")
-    	call("wpa_passphrase " + process[0] + " " + process[1] + "> " + wpa_configure_file ,shell=True)
+    	call("wpa_passphrase " + process[0] + " " + process[1] + "> " + Serial_wpa_configure_file ,shell=True)
 
+    	p = subprocess.Popen("service networking restart", stdout=subprocess.PIPE, stderr=subprocess.STDOUT,shell=True,preexec_fn=os.setsid)
+    	try:
+	    	for line in runProcess(p):
+	    		command_line = str(line)
+	    		if command_line.find("pid") == 0:
+	    			signal.signal(signal.SIGALRM, errorHandler)
+	    			signal.alarm(15)
+
+	    		if command_line.find("DHCPDISCOVER") ==0:
+	    			print('finded')
+	    			count = count + 1
+	    			if command_line.find("DHCPACK") == 0:
+	    				signal.alarm(0) # Alarm Off
+	    				print('command_line',command_line)
+	    				split_string = command_line.split("from ")
+	    				gateway_ip = split_string[1]
+	    				ip_set = True
+	    				print("[ROOT] gateway_ip : ",gateway_ip)
+
+	    	p = subprocess.Popen("ifconfig wlan0", stdout=subprocess.PIPE, stderr=subprocess.STDOUT,shell=True,preexec_fn=os.setsid)
+	    	for line in runProcess(p):
+	    		command_line = str(line)
+	    		if (command_line.find('cast:') == 36):
+	    			split1 = command_line.split(' ')
+	    			split2 = split1[11].split(':')
+	    			ipaddr = split2[1]
+
+	    	#ipaddr = gateway Address
+
+	    	if (is_network_alive(ipaddr)): # ping to gateway address (alive check)
+	    		client_sock.send("[BT] Wi-Fi Connected")
+	    		print("[ROOT] Wi-Fi Connected")
+	    	else:
+	    		client_sock.send("[BT] Failed to connect")
+	    		print("[ROOT] Wi-Fi Connected")
+
+
+	    except IOError: #Limit 15 Seconds
+			os.killpg(os.getpgid(p.pid), signal.SIGTERM)
+			print("Process Terminiated by Timeout (15 seconds)")
+			ap_mode = False
+
+    	ap_mode = False
+
+
+BluetoothReceiver()
 
