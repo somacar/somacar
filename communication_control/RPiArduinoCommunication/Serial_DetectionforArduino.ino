7bc6f4a21c0bd283698cf1e26eb55fd7a1a86213
DongGyu Kim <rainc.root@gmail.com>
2016-06-07 16:20:44 +0900

    add test code (라즈베리파이 <-> 아두이노 코드 테스트 끝낸 코드)

--- communication_control/RPiArduinoCommunication/Serial_DetectionforArduino.ino
+++ communication_control/RPiArduinoCommunication/Serial_DetectionforArduino.ino
@@ -3,8 +3,8 @@ const int ledPin = 13;
 void setup() {
    Serial.begin(9600);
    Serial.println("still alive inited");
-   pinMode(4, OUTPUT);
-   pinMode(11, OUTPUT);
+//   pinMode(4, OUTPUT);
+//   pinMode(11, OUTPUT);
 }
 
 void loop() {
@@ -16,51 +16,90 @@ void loop() {
 void r_funclist(int number) {
    switch (number) {
       case 0: // brake
-        Serial.println("Received 0 from Raspi");
+
         digitalWrite(3, HIGH);
-        delay(200);
-        digitalWrite(3, LOW);
+        
+        digitalWrite(4, LOW);
+        digitalWrite(5, LOW);
+        digitalWrite(6, LOW);
+        digitalWrite(8, LOW);
+        digitalWrite(9, LOW);
+        digitalWrite(11, LOW);
         break;
       case 1: // forward (fast)
-        Serial.println("Received 1 from Raspi");
+
         digitalWrite(4, HIGH);
-        delay(200);
-        digitalWrite(4, LOW);
+
+        digitalWrite(3, LOW);
+        digitalWrite(5, LOW);
+        digitalWrite(6, LOW);
+        digitalWrite(8, LOW);
+        digitalWrite(9, LOW);
+        digitalWrite(11, LOW);
         break;
       case 2: // left (fast)
-        Serial.println("Received 2 from Raspi");
+
         digitalWrite(5, HIGH);
-        delay(200);
-        digitalWrite(5, LOW);
+
+        digitalWrite(4, LOW);
+        digitalWrite(3, LOW);
+        digitalWrite(6, LOW);
+        digitalWrite(8, LOW);
+        digitalWrite(9, LOW);
+        digitalWrite(11, LOW);
+        
         break;
       case 3: // right (fast)
-        Serial.println("Received 3 from raspi");
         digitalWrite(6, HIGH);
-        delay(200);
-        digitalWrite(6, LOW);
+
+        digitalWrite(4, LOW);
+        digitalWrite(5, LOW);
+        digitalWrite(3, LOW);
+        digitalWrite(8, LOW);
+        digitalWrite(9, LOW);
+        digitalWrite(11, LOW);
         break;
       
       case 4: // forward (slow)
-        Serial.println("forward slow");
+
         digitalWrite(9, HIGH);
-        delay(200);
-        digitalWrite(9, LOW);
+
+
+        digitalWrite(4, LOW);
+        digitalWrite(5, LOW);
+        digitalWrite(3, LOW);
+        digitalWrite(8, LOW);
+        digitalWrite(11, LOW);
+        digitalWrite(6, LOW);
         break;
       case 5: // left (slow)
-        Serial.println("left slow");
         digitalWrite(11, HIGH);
-        delay(200);
-        digitalWrite(11, LOW);
+        
+
+        digitalWrite(4, LOW);
+        digitalWrite(5, LOW);
+        digitalWrite(3, LOW);
+        digitalWrite(8, LOW);
+        digitalWrite(6, LOW);
+        digitalWrite(9, LOW);
         break;
       case 6: // right(slow)
-        Serial.println("right slow");
         digitalWrite(8, HIGH);
-        delay(200);
-        digitalWrite(8, LOW);
+
+        digitalWrite(4, LOW);
+        digitalWrite(5, LOW);
+        digitalWrite(3, LOW);
+        digitalWrite(6, LOW);
+        digitalWrite(9, LOW);
+        digitalWrite(11,LOW);
+        
         break;
         
       default:
-        Serial.println("The Number is : " + number);
+
+        
         break;
    }
+
+
 }
