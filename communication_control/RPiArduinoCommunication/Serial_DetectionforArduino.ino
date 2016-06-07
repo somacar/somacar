ca2b7338e77c823b0ca1f9ee47664939983b5309
DongGyu Kim <rainc.root@gmail.com>
2016-06-07 14:40:44 +0900

    Update Serial_DetectionforArduino.ino

--- communication_control/RPiArduinoCommunication/Serial_DetectionforArduino.ino
+++ communication_control/RPiArduinoCommunication/Serial_DetectionforArduino.ino
@@ -4,6 +4,7 @@ void setup() {
    Serial.begin(9600);
    Serial.println("still alive inited");
    pinMode(4, OUTPUT);
+   pinMode(11, OUTPUT);
 }
 
 void loop() {
@@ -14,24 +15,50 @@ void loop() {
 
 void r_funclist(int number) {
    switch (number) {
-      case 0:
+      case 0: // brake
         Serial.println("Received 0 from Raspi");
-        digitalWrite(4, HIGH);
-        delay(1000);
-        digitalWrite(4, LOW);
+        digitalWrite(3, HIGH);
+        delay(200);
+        digitalWrite(3, LOW);
         break;
-      case 1:
+      case 1: // forward (fast)
         Serial.println("Received 1 from Raspi");
         digitalWrite(4, HIGH);
-        delay(1000);
+        delay(200);
         digitalWrite(4, LOW);
         break;
-      case 2:
+      case 2: // left (fast)
         Serial.println("Received 2 from Raspi");
-        digitalWrite(4, HIGH);
-        delay(1000);
-        digitalWrite(4, LOW);
+        digitalWrite(5, HIGH);
+        delay(200);
+        digitalWrite(5, LOW);
         break;
+      case 3: // right (fast)
+        Serial.println("Received 3 from raspi");
+        digitalWrite(6, HIGH);
+        delay(200);
+        digitalWrite(6, LOW);
+        break;
+      
+      case 4: // forward (slow)
+        Serial.println("forward slow");
+        digitalWrite(9, HIGH);
+        delay(200);
+        digitalWrite(9, LOW);
+        break;
+      case 5: // left (slow)
+        Serial.println("left slow");
+        digitalWrite(11, HIGH);
+        delay(200);
+        digitalWrite(11, LOW);
+        break;
+      case 6: // right(slow)
+        Serial.println("right slow");
+        digitalWrite(8, HIGH);
+        delay(200);
+        digitalWrite(8, LOW);
+        break;
+        
       default:
         Serial.println("The Number is : " + number);
         break;
