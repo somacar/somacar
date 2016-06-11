1339a76a50761ec5cfae082fadc499013eb2656f
LeeYoungIn <pimery@nate.com>
2016-06-11 11:50:12 +0900

    정사각형 안에 정사각형

--- image_processing/project/target.cpp
+++ image_processing/project/target.cpp
@@ -9,8 +9,8 @@ Target::Target() {
 
 void Target::init(UMat u) {
     this->draw = u.clone();
-    cvtColor(u.clone(), u, COLOR_RGB2GRAY);
     this->orig = u.clone();
+    cvtColor(u.clone(), u, COLOR_RGB2GRAY);
     //threshold(u, u, 128, 255, THRESH_BINARY | THRESH_OTSU);
     //inRange(f, Scalar(0, 0, 0, 0), Scalar(160, 255, 30, 0), f);
     GaussianBlur(u, u, Size(7, 7), 1.5, 1.5);
@@ -23,22 +23,47 @@ bool Target::is_square(vector<Point> c, Rect *rect) {
     *rect = boundingRect(this->approx);
     if ((rect->width < MIN_RECT) || (rect->height < MIN_RECT)) return false; // min size
     this->dist = ORIG_WIDTH * ORIG_F / rect->width;
+    this->out_width = rect->width;
     if (rect->width / (float) rect->height < 0.8 || rect->width / (float) rect->height > 1.2) return false; // ratio
     vector<Point> hull;
     convexHull(c, hull);
     return ((contourArea(c) / (float) contourArea(hull)) > 0.9);
 }
 
-bool Target::is_star(UMat u) {
-    double divMaxSize = 0.175, divMinSize = 0.125;
-    vector<vector<Point> > contours;
-    vector<Vec4i> hierarchy;
+bool Target::is_rect(vector<Point> c) {
+    if (c.size() != 4) return false; // rectangular
+    Rect rect = boundingRect(c);
+    if ((rect.width < MIN_RECT) || (rect.height < MIN_RECT)) return false; // min size
+    if (rect.width / (float) rect.height < 0.8 || rect.width / (float) rect.height > 1.2) return false; // ratio
+    if (this->out_width / (float) rect.width < 0.8 || this->out_width / (float) rect.width > 1.2) return false; // ratio
+    vector<Point> hull;
+    convexHull(c, hull);
+    return ((contourArea(c) / (float) contourArea(hull)) > 0.9);
+}
 
-    threshold(u, u, 100, 255, 0);
-    findContours(u, contours, hierarchy, CV_RETR_TREE, CV_CHAIN_APPROX_SIMPLE, Point(0, 0));
-    for (int i=0; i < contours.size(); i++) {
-        if(sqrt(contourArea(contours[i]))/arcLength(contours[i], true ) < divMaxSize && sqrt(contourArea(contours[i]))/arcLength(contours[i], true) > divMinSize)
+bool Target::is_star(UMat u) {
+//    double divMaxSize = 0.175, divMinSize = 0.125;
+//    vector<vector<Point> > contours;
+//    vector<Vec4i> hierarchy;
+//    cvtColor(u, u, COLOR_RGB2GRAY);
+//    threshold(u, u, 100, 255, 0);
+//    findContours(u, contours, hierarchy, CV_RETR_TREE, CV_CHAIN_APPROX_SIMPLE, Point(0, 0));
+//    Scalar color = Scalar( rngee.uniform(0, 255), rngee.uniform(0,255), rngee.uniform(0,255));
+//    for (int i=0; i < contours.size(); i++) {
+//        if(sqrt(contourArea(contours[i]))/arcLength(contours[i], true ) < divMaxSize && sqrt(contourArea(contours[i]))/arcLength(contours[i], true) > divMinSize) {
+//            drawContours(this->draw, contours, i, color, 2, 8, hierarchy, 0, Point() );
+//            return true;
+//        }
+//    }
+
+    bitwise_not(u, u);
+    vector<vector<Point>> contours;
+    findContours(u, contours, RETR_EXTERNAL, CHAIN_APPROX_SIMPLE);
+
+    for (auto const &c: contours) {
+        if (is_rect(c)) {
             return true;
+        }
     }
     return false;
 }
@@ -107,7 +132,8 @@ bool Target::find_square(UMat *sqr) {
     for (auto const &c: contours) {
         approxPolyDP(c, this->approx, 0.01 * arcLength(c, true), true);
         if (is_square(c, &rect)) {
-            *sqr = this->orig(rect);
+	    if (MODE == TARGET_STAR) *sqr = this->cvt(rect);
+            else *sqr = this->orig(rect);
 
             vector<Point2f> corn_pt, quad_pt;
             for (int i = 0; i < this->approx.size(); i++)
@@ -118,7 +144,10 @@ bool Target::find_square(UMat *sqr) {
             quad_pt.push_back(Point2f(rect.width, 0));
 
             UMat tr = getPerspectiveTransform(corn_pt, quad_pt).getUMat(ACCESS_READ);
-            warpPerspective(this->orig, *sqr, tr, sqr->size());
+	    if (MODE == TARGET_STAR)
+            	warpPerspective(this->orig.clone(), *sqr, tr, sqr->size());
+            else
+		warpPerspective(this->cvt.clone(), *sqr, tr, sqr->size());
             return true;
         }
     }
