95fceb94cfe2a6d972942edc625eefa70b5618c4
GunHee Choi <GunHee Choi>
2016-06-13 21:21:39 +0900

    pretest 소스코드 보기 좋게 수정

--- pretest/bluetooth/bluetooth.ino
+++ pretest/bluetooth/bluetooth.ino
@@ -1,44 +1,26 @@
 #include <SoftwareSerial.h>
 
-SoftwareSerial BT(3, 2);
+SoftwareSerial BT(11, 10); //Tx, Rx
+int ledPin = 2;
+char inChar;
 
 void setup() {
-  pinMode(13, OUTPUT);
+  pinMode(ledPin, OUTPUT);
 
   BT.begin(9600);
-
   BT.println("Hello from Arduino");
 }
 
-byte a;
-
 void loop() {
   if(BT.available()) {
-    a = BT.read();
-    BT.write(a);
-    if( a == 1 ) {
-      BT.write(a);
+    inChar = BT.read();
+    BT.write(inChar);
+    if( inChar == '1' ) {
+      BT.write(inChar);
+      digitalWrite(ledPin, HIGH);
     } else {
-      BT.write(0x23);
+      digitalWrite(ledPin, LOW);
+      BT.write(inChar);
     }
-      
-    
-    /*a = (BT.read());
-    if(a == '1') {
-      digitalWrite(13, HIGH);
-      BT.println("LED on");
-    }
-
-    if(a == '2') {
-      digitalWrite(13, LOW);
-      BT.println("LED off");
-    }
-
-    if(a =='?') {
-      BT.println("Send '1' to turn LED on");
-      BT.println("Send '2' to turn LED on");
-      
-    }
-    */
   }//end of BT.available()
 }//end of loop()
